4ec18d154ab2bb2b3346fda8f8b407c8d53bc7cc
Bill Rossum <billrossum@gmail.com>
2020-03-26 23:06:20 -0500

    I give up on this stupid AWS library. . .

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
  */
 
 import './shim.js';
-// import crypto from 'crypto';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import crypto from 'isomorphic-webcrypto';
+
+crypto.ensureSecure().then(() => {
+    global.crypto = crypto;
+
+    AppRegistry.registerComponent(appName, () => App);
+});
