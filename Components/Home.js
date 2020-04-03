1a9db9b148f172fa41a4e22417a0d76047be9458
Bill Rossum <billrossum@gmail.com>
2020-04-02 21:53:07 -0500

    use scaledrone

--- Components/Home.js
+++ Components/Home.js
@@ -1,4 +1,5 @@
 // based off https://github.com/colinwitkamp/react-native-webrtc-sample/blob/master/App.js
+// and https://www.scaledrone.com/blog/webrtc-tutorial-simple-video-chat
 
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
@@ -16,72 +17,181 @@ import {
 
 import strings from '../strings';
 import {uuidv4} from '../util';
-import * as KVSWebRTC from 'amazon-kinesis-video-streams-webrtc';
-import AWS from 'aws-sdk';
+
+import Scaledrone from 'scaledrone-react-native';
 
 const isFront = true; // Use Front camera?
 
-
-// DescribeSignalingChannel API can also be used to get the ARN from a channel name.
-const channelARN = 'arn:aws:kinesisvideo:us-east-1:838649631748:channel/testing/1581877891049';
-
-// AWS Credentials
-const accessKeyId = 'AKIAJB7DYLR6Q2QMAUEQ';
-const secretAccessKey = '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';
-
-const region = 'us-east-1';
 const clientId = uuidv4();
 
-const iceServers = [
-    { urls: `stun:stun.kinesisvideo.${region}.amazonaws.com:443` }
-];
-
-const DEFAULT_ICE = {
-// we need to fork react-native-webrtc for relay-only to work.
-//  iceTransportPolicy: "relay",
-    iceServers
+const configuration = {
+    iceServers: [{
+        urls: 'stun:stun.l.google.com:19302' // Google's public STUN server
+    }]
 };
 
-const kinesisVideoClient = new AWS.KinesisVideo({
-    region,
-    accessKeyId,
-    secretAccessKey,
-});
+function onSuccess() {
+
+}
+
+function onError(error) {
+    console.error(error);
+}
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
 
-        // Bind stuff
-        this.setupSignalingClient = this.setupSignalingClient.bind(this);
-        this.setupWebRTC = this.setupWebRTC.bind(this);
-        this.handleConnect = this.handleConnect.bind(this);
-        this.onIceConnectionStateChange = this.onIceConnectionStateChange.bind(this);
-        this.onAddStream = this.onAddStream.bind(this);
-        this.onIceCandidate = this.onIceCandidate.bind(this);
+        // Method bindings
+        this.setupRoom = this.setupRoom.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.startWebRTC = this.startWebRTC.bind(this);
+        this.startListeningToSignals = this.startListeningToSignals.bind(this);
+        this.localDescCreated = this.localDescCreated.bind(this);
 
-        this.signalingClient = null;
-        this.setupSignalingClient()
-            .then(() => {
-                console.log("Finished setting up signaling client.");
-            })
-            .catch((e) => {
-                console.error("Error", e);
-        });
+        this.drone = new Scaledrone('S2ktEkKPVRBiKCka');
+
+        this.setupRoom();
 
         this.localStream = null;
-
-        this.peer = null;
 
         this.state = {
             connected: false,
             localStreamURL: null,
             remoteStreamURL: null,
-
-            iceConnectionState: null,
-            pendingCandidates: [],
-            offerReceived: false
         };
+    }
+
+    setupRoom() {
+        this.roomHash = uuidv4();
+        this.roomName = 'observable-de0798';
+
+        this.room = this.drone.subscribe(this.roomName);
+
+        this.room.on('open', (error) => {
+            if (error) {
+                return onError(error);
+            }
+        });
+
+        this.room.on('members', (members) => {
+            // if (members.length > 2) {
+            //     console.error("Room is full.");
+            //     this.room.unsubscribe();
+            // }
+
+            const isOfferer = members.length === 2;
+            this.startWebRTC(isOfferer);
+            this.startListeningToSignals();
+        });
+    }
+
+    sendMessage(message) {
+        this.drone.publish({
+            room: this.roomName,
+            message
+        });
+    }
+
+    startWebRTC(isOfferer) {
+        const peer = new RTCPeerConnection(configuration);
+
+        peer.onicecandidate = (event) => {
+            if (event.candidate) {
+                this.sendMessage({
+                    'candidate': event.candidate
+                });
+            }
+        };
+
+        // only offerer handles negotiation needed
+        if (isOfferer) {
+            peer.onnegotiationneeded = () => {
+                peer.createOffer().then(this.localDescCreated).catch(onError);
+            }
+        }
+
+        peer.onaddstream = (event) => {
+            this.setState({
+                remoteStreamURL: event.stream.toURL()
+            });
+        };
+
+        // Setup Camera & Audio
+        mediaDevices.enumerateDevices().then(sourceInfos => {
+            let videoSourceId;
+            for (let i = 0; i < sourceInfos.length; i++) {
+                const sourceInfo = sourceInfos[i];
+                if(sourceInfo.kind === "videoinput" && sourceInfo.facing === (isFront ? "front" : "environment")) {
+                    videoSourceId = sourceInfo.deviceId;
+                }
+            }
+            mediaDevices.getUserMedia({
+                audio: true,
+                video: {
+                    mandatory: {
+                        minWidth: 100, // Provide your own width, height and frame rate here
+                        minHeight: 100,
+                        minFrameRate: 30
+                    },
+                    facingMode: (isFront ? "user" : "environment"),
+                    optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+                }
+            })
+                .then((stream) => {
+                    // Got stream!
+                    console.log("Got video device.");
+                    this.setState({
+                        localStreamURL: stream.toURL()
+                    });
+                    this.localStream = stream;
+                    peer.addStream(stream);
+                })
+                .catch(onError);
+        });
+
+        this.peer = peer;
+    }
+
+    startListeningToSignals() {
+        this.room.on('data', (message, client) => {
+            // We sent the message
+            if (!client || client.id === this.drone.clientId) {
+                return;
+            }
+
+            if (message.sdp) {
+                // Called after receiving offer or answer from peer
+                this.peer.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                    // Received offer, so answer it
+                    if (this.peer.remoteDescription.type === 'offer') {
+                        this.peer.createAnswer().then(localDescCreated).catch(onError);
+                    }
+                }, onError);
+            } else if (message.candidate) {
+                this.peer.addIceCandidate(
+                    new RTCIceCandidate(message.candidate),
+                    onSuccess,
+                    onError
+                );
+            }
+        });
+    }
+
+    localDescCreated(desc) {
+        this.peer.setLocalDescription(
+            desc,
+            () => this.sendMessage({
+                'sdp': this.peer.localDescription
+            }),
+            onError
+        );
+    }
+
+    componentWillUnmount() {
+        // Leave room and close connection to scaledrone
+        this.room.unsubscribe();
+        this.drone.close();
     }
 
     render() {
@@ -110,217 +220,22 @@ export default class Home extends Component {
                 </View>
                 <View style={ this.state.connected ? styles.onlineCircle : styles.offlineCircle}/>
                 <View>
-                    <TouchableOpacity onPress={this.handleConnect} disabled={this.state.offer_received}>
-                        <Text>
-                            Connect
-                        </Text>
-                    </TouchableOpacity>
+                    {/*<TouchableOpacity onPress={this.handleConnect} disabled={this.state.offer_received}>*/}
+                    {/*    <Text>*/}
+                    {/*        Connect*/}
+                    {/*    </Text>*/}
+                    {/*</TouchableOpacity>*/}
                     { // Offer received and offer not answered
-                        (this.state.offer_received && !this.state.offer_answered) &&
-                        <TouchableOpacity onPress={this.handleAnswer}>
-                            <Text>
-                                Answer
-                            </Text>
-                        </TouchableOpacity>
+                        // (this.state.offer_received && !this.state.offer_answered) &&
+                        // <TouchableOpacity onPress={this.handleAnswer}>
+                        //     <Text>
+                        //         Answer
+                        //     </Text>
+                        // </TouchableOpacity>
                     }
                 </View>
             </View>
         );
-    }
-
-    componentDidMount() {
-        // Setup Camera & Audio
-        mediaDevices.enumerateDevices().then(sourceInfos => {
-            let videoSourceId;
-            for (let i = 0; i < sourceInfos.length; i++) {
-                const sourceInfo = sourceInfos[i];
-                if(sourceInfo.kind === "videoinput" && sourceInfo.facing === (isFront ? "front" : "environment")) {
-                    videoSourceId = sourceInfo.deviceId;
-                }
-            }
-            mediaDevices.getUserMedia({
-                audio: true,
-                video: {
-                    mandatory: {
-                        minWidth: 100, // Provide your own width, height and frame rate here
-                        minHeight: 100,
-                        minFrameRate: 30
-                    },
-                    facingMode: (isFront ? "user" : "environment"),
-                    optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
-                }
-            })
-                .then((stream) => {
-                    // Got stream!
-                    console.log("Got video device.");
-                    this.setState({
-                        localStreamURL: stream.toURL()
-                    });
-                    this.localStream = stream
-                })
-                .catch(error => {
-                    // Log error
-                    console.log(error);
-                });
-        });
-    }
-
-    async setupSignalingClient() {
-        const getSignalingChannelEndpointResponse = await kinesisVideoClient
-            .getSignalingChannelEndpoint({
-                ChannelARN: channelARN,
-                SingleMasterChannelEndpointConfiguration: {
-                    Protocols: ['WSS', 'HTTPS'],
-                    Role: KVSWebRTC.Role.VIEWER,
-                },
-            })
-            .promise();
-        const endpointsByProtocol = getSignalingChannelEndpointResponse.ResourceEndpointList.reduce((endpoints, endpoint) => {
-            endpoints[endpoint.Protocol] = endpoint.ResourceEndpoint;
-            return endpoints;
-        }, {});
-
-        this.signalingClient = new KVSWebRTC.SignalingClient({
-            channelARN,
-            channelEndpoint: endpointsByProtocol.WSS,
-            clientId,
-            role: KVSWebRTC.Role.VIEWER,
-            region,
-            credentials: {
-                accessKeyId,
-                secretAccessKey,
-            },
-        });
-
-        this.signalingClient.on('open', async () => {
-            console.info("Signaling client opened.");
-
-            // Create an SDP offer and send it to the master
-            const offer = await this.peer.createOffer({
-                offerToReceiveAudio: true,
-                offerToReceiveVideo: true,
-            });
-            await this.peer.setLocalDescription(offer);
-            this.signalingClient.sendSdpOffer(this.peer.localDescription);
-        });
-
-        // When the SDP answer is received back from the master, add it to the peer connection.
-        this.signalingClient.on('sdpAnswer', async answer => {
-            await this.peer.setRemoteDescription(answer);
-        });
-
-        // When an ICE candidate is received from the master, add it to the peer connection.
-        this.signalingClient.on('iceCandidate', candidate => {
-            this.peer.addIceCandidate(candidate);
-        });
-
-        this.signalingClient.on('close', () => {
-            // Handle client closures
-            console.info("Signaling client received close.");
-        });
-
-        this.signalingClient.on('error', error => {
-            // Handle client errors
-            console.error(error);
-        });
-
-        console.info("Done setting up signaling client.");
-    }
-
-    async setupWebRTC() {
-        const peer = new RTCPeerConnection(DEFAULT_ICE);
-
-        // peer.oniceconnectionstatechange = this.onIceConnectionStateChange;
-        // peer.onaddstream = this.onAddStream;
-        // peer.onicecandidate = this.onIceCandidate;
-
-        // Send any ICE candidates generated by the peer connection to the other peer
-        peer.addEventListener('icecandidate', ({ candidate }) => {
-            if (candidate) {
-                this.signalingClient.sendIceCandidate(candidate);
-            } else {
-                // No more ICE candidates will be generated
-            }
-        });
-
-        // As remote tracks are received, add them to the remote view
-        peer.addEventListener('track', event => {
-            if (this.remoteStream) {
-                return;
-            }
-            this.remoteStream = event.streams[0];
-        });
-
-        peer.addStream(this.localStream);
-
-        this.peer = peer;
-
-        this.signalingClient.open();
-    }
-
-    async handleConnect(e) {
-        console.log(crypto);
-        await this.setupWebRTC();
-    }
-
-    onIceConnectionStateChange(e) {
-        console.info('ICE Connection State Changed:', e.target.iceConnectionState);
-
-        this.setState({
-            ice_connection_state: e.target.iceConnectionState
-        });
-
-        switch (e.target.iceConnectionState) {
-            case 'closed':
-            case 'disconnected':
-            case 'failed':
-                if (this.peer) {
-                    this.peer.close();
-                    this.setState({
-                        remoteStreamURL: null
-                    });
-                    this.remoteStream = null
-                }
-                break
-        }
-    }
-
-    onAddStream(e) {
-        console.info('Remote Stream Added:', e.stream);
-        this.setState({
-            remoteStreamURL: e.stream.toURL()
-        });
-        this.remoteStream = e.stream;
-    }
-
-    onIceCandidate(e) {
-        const { candidate } = e;
-        console.info('Ice candidate found:', candidate);
-
-        if (candidate) {
-            let pendingRemoteIceCandidates = this.state.pendingCandidates;
-            if (Array.isArray(pendingRemoteIceCandidates)) {
-                this.setState({
-                    pendingCandidates: [...pendingRemoteIceCandidates, candidate]
-                });
-            } else {
-                this.setState({
-                    pendingCandidates: [candidate]
-                })
-            }
-        } else {
-            if (this.state.pendingCandidates.length > 1) {
-                this.sendMessage({
-                    type: this.state.offerReceived ? 'answer' : 'offer',
-                    payload: {
-                        description: this.peer.localDescription,
-                        candidates: this.state.pendingCandidates
-                    }
-                })
-            } else {
-                console.error('No candidates found.');
-            }
-        }
     }
 }
 
--- index.js
+++ index.js
@@ -2,16 +2,8 @@
  * @format
  */
 
-import './shim.js';
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import crypto from 'isomorphic-webcrypto';
-
-crypto.ensureSecure().then(() => {
-    global.crypto = crypto;
-
-    AppRegistry.registerComponent(appName, () => App);
-});
+AppRegistry.registerComponent(appName, () => App);
